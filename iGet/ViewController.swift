6dfec030010cf2423d7d03db8d7ca7d83680ff53
@rigodev <nn@ya.ru>
2019-06-12 13:16:23 +0300

    Initial Commit

---
+++ iGet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iGet
+//
+//  Created by rigo on 12/06/2019.
+//  Copyright © 2019 dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
